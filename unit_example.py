435a5098be4e738fffc701e0deb0c1666ddabce7
rathma <wrathofrathma@gmail.com>
2020-02-25 07:12:24 -0500

    f

--- unit_example.py
+++ unit_example.py
@@ -16,7 +16,20 @@ cube_radius = 0.5  # Radius, giving us planes on even coordinates
 cr = cube_radius
 cwo = cube_world_origin
 
-cube_verts = np.array(
+cube1_verts = np.array(
+    [
+        cwo - cr,
+        cwo - np.array((-cr, cr, cr)),
+        cwo - np.array((-cr, -cr, cr)),
+        cwo - np.array((cr, -cr, cr)),
+        cwo - np.array((cr, cr, -cr)),
+        cwo - np.array((-cr, cr, -cr)),
+        cwo - np.array((-cr, -cr, -cr)),
+        cwo - np.array((cr, -cr, -cr,)),
+    ]
+)
+cwo = np.array((1.5, 1.5, 2.5))
+cube2_verts = np.array(
     [
         cwo - cr,
         cwo - np.array((-cr, cr, cr)),
@@ -29,13 +42,21 @@ cube_verts = np.array(
     ]
 )
 
-cube_faces = [
-    [cube_verts[2], cube_verts[3], cube_verts[7], cube_verts[6]],  # back
-    [cube_verts[0], cube_verts[1], cube_verts[5], cube_verts[4]],  # front
-    [cube_verts[0], cube_verts[1], cube_verts[2], cube_verts[3]],  # Bottom
-    [cube_verts[4], cube_verts[5], cube_verts[6], cube_verts[7]],  # top
-    [cube_verts[4], cube_verts[7], cube_verts[3], cube_verts[0]],  # left
-    [cube_verts[1], cube_verts[2], cube_verts[6], cube_verts[5]],  # right
+cube1_faces = [
+    [cube1_verts[2], cube1_verts[3], cube1_verts[7], cube1_verts[6]],  # back
+    [cube1_verts[0], cube1_verts[1], cube1_verts[5], cube1_verts[4]],  # front
+    [cube1_verts[0], cube1_verts[1], cube1_verts[2], cube1_verts[3]],  # Bottom
+    [cube1_verts[4], cube1_verts[5], cube1_verts[6], cube1_verts[7]],  # top
+    [cube1_verts[4], cube1_verts[7], cube1_verts[3], cube1_verts[0]],  # left
+    [cube1_verts[1], cube1_verts[2], cube1_verts[6], cube1_verts[5]],  # right
+]
+cube2_faces = [
+    [cube2_verts[2], cube2_verts[3], cube2_verts[7], cube2_verts[6]],  # back
+    [cube2_verts[0], cube2_verts[1], cube2_verts[5], cube2_verts[4]],  # front
+    [cube2_verts[0], cube2_verts[1], cube2_verts[2], cube2_verts[3]],  # Bottom
+    [cube2_verts[4], cube2_verts[5], cube2_verts[6], cube2_verts[7]],  # top
+    [cube2_verts[4], cube2_verts[7], cube2_verts[3], cube2_verts[0]],  # left
+    [cube2_verts[1], cube2_verts[2], cube2_verts[6], cube2_verts[5]],  # right
 ]
 ################################################
 # Box stuff (Hard coding the verts since I don't feel like porting the rotations yet)
@@ -48,7 +69,12 @@ box_verts = np.array(
     ]
 )
 bv = box_verts
-be = [[bv[0], bv[1]], [bv[1], bv[2]], [bv[2], bv[3]], [bv[3], bv[0]]]
+be = [
+    [bv[0], bv[1]],  # Right
+    [bv[1], bv[2]],  # bottom
+    [bv[2], bv[3]],  # left
+    [bv[3], bv[0]],  # top
+]
 box_edges = be
 box_face = [[bv[0], bv[1], bv[2], bv[3]]]
 #########################################################################
@@ -65,7 +91,7 @@ planes = [
     plane.create_yz(distance=1),  # left
     plane.create_yz(distance=2),  # right
 ]
-intersections = [
+intersections1 = [
     False,  # back
     False,  # front
     False,  # bottom
@@ -73,20 +99,30 @@ intersections = [
     False,  # left
     False,  # right
 ]
-ray_intersects = []
-v_intersect = []
+ray_intersects1 = []
+v_intersect1 = []
+intersections2 = [
+    False,  # back
+    False,  # front
+    False,  # bottom
+    False,  # top
+    False,  # left
+    False,  # right
+]
+ray_intersects2 = []
+v_intersect2 = []
 # For each line in our mesh(our square)....
 # -- Check intersections against every cube that's not collided with(our singular cube).
 for r in box_rays:
     for i in range(6):
-        if not intersections[i]:
+        if not intersections1[i]:
             isec = geometric_tests.ray_intersect_plane(r, planes[i])
             if isec is not None:
                 # Need to check the bounds
                 print(isec)
-                ys = [y[1] for y in cube_faces[i]]
-                xs = [x[0] for x in cube_faces[i]]
-                zs = [z[2] for z in cube_faces[i]]
+                ys = [y[1] for y in cube1_faces[i]]
+                xs = [x[0] for x in cube1_faces[i]]
+                zs = [z[2] for z in cube1_faces[i]]
                 maxy = max(ys)
                 maxx = max(xs)
                 maxz = max(zs)
@@ -102,14 +138,46 @@ for r in box_rays:
                 print("Intersection found on i=" + str(i))
                 print("ray: " + str(r))
                 print("Isec: " + str(isec))
-                intersections[i] = True
-                ray_intersects += [r]
-                v_intersect += [isec]
+                intersections1[i] = True
+                ray_intersects1 += [r]
+                v_intersect1 += [isec]
 
-print("Number of intersections found: " + str(len(ray_intersects)))
-for r in ray_intersects:
+for r in box_rays:
+    for i in range(6):
+        if not intersections2[i]:
+            isec = geometric_tests.ray_intersect_plane(r, planes[i])
+            if isec is not None:
+                # Need to check the bounds
+                print(isec)
+                ys = [y[1] for y in cube2_faces[i]]
+                xs = [x[0] for x in cube2_faces[i]]
+                zs = [z[2] for z in cube2_faces[i]]
+                maxy = max(ys)
+                maxx = max(xs)
+                maxz = max(zs)
+                miny = min(ys)
+                minx = min(xs)
+                minz = min(zs)
+                if isec[0] < minx or isec[0] > maxx:
+                    continue
+                if isec[1] < miny or isec[1] > maxy:
+                    continue
+                if isec[2] < minz or isec[2] > maxz:
+                    continue
+                print("Intersection found on i=" + str(i))
+                print("ray: " + str(r))
+                print("Isec: " + str(isec))
+                intersections2[i] = True
+                ray_intersects2 += [r]
+                v_intersect2 += [isec]
+print("Number of intersections found on c1: " + str(len(ray_intersects1)))
+for r in ray_intersects1:
     print(r)
-print(v_intersect)
+print(v_intersect1)
+print("Number of intersections found on c2: " + str(len(ray_intersects2)))
+for r in ray_intersects2:
+    print(r)
+print(v_intersect2)
 
 ########################################################################
 # Matplotlib stuff
@@ -117,14 +185,23 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection="3d")
 r = [-4, 4]
 X, Y = np.meshgrid(r, r)
-ax.scatter3D(cube_verts[:, 0], cube_verts[:, 1], cube_verts[:, 2])
+ax.scatter3D(cube1_verts[:, 0], cube1_verts[:, 1], cube1_verts[:, 2])
 
-cf = []
+cf1 = []
 for i in range(6):
-    if intersections[i] == False:
-        cf += [cube_faces[i]]
+    if intersections1[i] == False:
+        cf1 += [cube1_faces[i]]
 ax.add_collection3d(
-    Poly3DCollection(cf, facecolors="cyan", linewidths=1, edgecolors="r", alpha=0.25,)
+    Poly3DCollection(cf1, facecolors="cyan", linewidths=1, edgecolors="r", alpha=0.25,)
+)
+cf2 = []
+for i in range(6):
+    if intersections2[i] == False:
+        cf2 += [cube2_faces[i]]
+ax.add_collection3d(
+    Poly3DCollection(
+        cf2, facecolors="purple", linewidths=1, edgecolors="r", alpha=0.25,
+    )
 )
 ax.add_collection3d(
     Poly3DCollection(
